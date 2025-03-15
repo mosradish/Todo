fa1f66345d1712f0d6d3990af830dc8506e1c0e4
EC2 Default User <ec2-user@ip-172-31-43-115.ap-northeast-1.compute.internal>
2025-03-15 16:29:24 +0000

    Local changes before pulling

---
+++ backend/test_db.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+DB_NAME = "postgres"
+DB_USER = "mosradish"
+DB_PASS = "Saru9240%"
+DB_HOST = "tododb.cja2o0uecn6b.ap-northeast-1.rds.amazonaws.com"
+DB_PORT = "5432"
+
+try:
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+    print("✅ 接続成功!")
+    conn.close()
+except Exception as e:
+    print("❌ 接続失敗:", e)
